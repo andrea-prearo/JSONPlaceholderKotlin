c5d078c88da8261beb354ef6d4ce5139790db853
Andrea Prearo <andrea.prearo@gmail.com>
2020-07-06 08:11:56 -0700

    Rename androidApp to app

---
+++ app/build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+}
+
+android {
+    compileSdkVersion(Versions.compile_sdk)
+    buildToolsVersion = Versions.buildToolsVersion
+    defaultConfig {
+        applicationId = "com.aprearo.jsonplaceholder.sampleapp"
+        minSdkVersion(Versions.min_sdk)
+        targetSdkVersion(Versions.target_sdk)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    packagingOptions {
+        exclude("META-INF/*.kotlin_module")
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk7", KotlinCompilerVersion.VERSION))
+    implementation(project(":shared"))
+    implementation(Deps.recyclerView)
+    implementation(Deps.material_x)
+    implementation(Deps.app_compat_x)
+    implementation(Deps.core_ktx)
+    implementation(Deps.Ktor.androidCore)
+    implementation(Deps.constraintlayout)
+    implementation(Deps.Coroutines.jdk)
+    implementation(Deps.Coroutines.android)
+    testImplementation(Deps.junit)
+}
